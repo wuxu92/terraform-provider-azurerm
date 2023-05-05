02a16bc12165cf57b520efa381107eade6648f64
xuwu1 <xuwu1@microsoft.com>
2023-05-05 14:53:17 +0800

    use Fg instead of Bg in colorful output

--- internal/tools/document-lint/mdparse/util/colors.go
+++ internal/tools/document-lint/mdparse/util/colors.go
@@ -5,8 +5,8 @@ import "github.com/fatih/color"
 var (
 	Bold       = color.New(color.Bold).Sprint
 	ItalicCode = color.New(color.Italic, color.FgCyan).Sprint
-	FormatCode = color.New(color.BgMagenta).Sprint
-	Blue       = color.New(color.BgBlue).Sprint
-	IssueLine  = color.New(color.BgYellow).Sprint
-	FixedCode  = color.New(color.BgGreen).Sprint
+	FormatCode = color.New(color.FgMagenta).Sprint
+	Blue       = color.New(color.FgBlue).Sprint
+	IssueLine  = color.New(color.FgYellow).Sprint
+	FixedCode  = color.New(color.FgGreen).Sprint
 )
